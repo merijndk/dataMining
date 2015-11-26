92bc33d762b46dd4a77852ce3bf6f26e3b463e0b
merijn de klerk <merijndk@gmail.com>
2015-11-26 11:52:37 +0100

    Titel en omschrijving

--- check.py
+++ check.py
@@ -1,4 +1,5 @@
 import urllib2
+from bs4 import BeautifulSoup
 import random
 import string
 
@@ -8,18 +9,40 @@ letters = "abcdefghijklmnopqrstuvwxyz"
 for j in "abcdefghijklm":
 	for k in letters:
 		for l in letters:
-			url = "http://www."+i+j+k+l+".com"
-			print url
+			url = "http://www."+j+k+l+".com"
 			try:
-			    urllib2.urlopen(url)
-			    with open("output.txt", "a") as myfile:
-			   		myfile.write(url + "\n")
-			   		b+=1
-			   		print b
+				rawdata = urllib2.urlopen(url)
+				soup = BeautifulSoup(rawdata, 'html.parser')
+				title = soup.title.string
+				print title
+
+				desc = soup.find(attrs={"name":"description"}) 
+				if desc:
+					desc = desc.encode('utf-8')
+				else:
+					desc = "geen omschrijving gevonden"
+				
+
+				with open("output.txt", "a") as myfile:
+					myfile.write("url: " + url + "\n"  + "Omshcrijving: " + desc + "\n" + "Title: " + title + "\n\n")
+					b+=1
+					print b
 			except:
 				pass
 
 
-
+# for j in "abcdefghijklm":
+# 	for k in letters:
+# 		for l in letters:
+# 			url = "http://www."+j+k+l+".com"
+# 			print url
+# 			try:
+# 			    urllib2.urlopen(url)
+# 			    with open("output.txt", "a") as myfile:
+# 			   		myfile.write(url + "\n")
+# 			   		b+=1
+# 			   		print b
+# 			except:
+# 				pass
 
 
